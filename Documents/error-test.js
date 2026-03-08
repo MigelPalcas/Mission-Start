d7cc8daee4f6b5ecf18f28dce4ce444b2c458237
Miguel Palma C. <lpalmacas@outlook.com>
2026-03-08 01:01:16 +0100

    Se ha realizado el 2do issue y se ha creado la carpeta documentos e img para documentar todo los realizado

---
+++ Documents/error-test.js
@@ -0,0 +1,15 @@
+// Archivo con errores para probar ESLint
+
+const nombre = "Miguel"
+
+function saludar() {
+console.log("Hola " + nombre)
+}
+
+saludar()
+
+let edad = 25
+
+edad = "veinticinco"
+
+console.log(mensaje)
